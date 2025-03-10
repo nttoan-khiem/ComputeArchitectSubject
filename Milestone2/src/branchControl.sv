4941498f8e2ca7b070da8093779d85274a2a10de
nttoan-khiem <nttkhiem2510@gmail.com>
2025-03-10 22:14:02 +0700

    update branch control tb

--- Milestone2/src/branchControl.sv
+++ Milestone2/src/branchControl.sv
@@ -1,42 +1,56 @@
-module branchControl(
-    input logic [31:0] i_rs1Data,
-    input logic [31:0] i_rs2Data,
-    input logic i_brUnsign,
-    output logic o_brLess,
-    output logic o_brEqual
+`timescale 1ps/1ps
+
+module branchControl (
+    input  [31:0] i_rs1Data,
+    input  [31:0] i_rs2Data,
+    input         i_brUnsign,
+    output        o_brLess,
+    output        o_brEqual
 );
-logic [32:0] rs1, rs2;
-assign rs1 = {1'b0, i_rs1Data};
-assign rs2 = {1'b0, i_rs2Data};
-xorLogic xorLogicBlock(
-    .i_rs1(i_rs1),
-    .i_rs2(i_rs2),
-    .o_brEqual(o_brEqual)
-);
+
+    wire [32:0] rs1, rs2;
+
+    assign rs1 = {1'b0, i_rs1Data}; // extern zero to become 33 bit number 
+    assign rs2 = {1'b0, i_rs2Data};
+
+    xorLogic xorLogicBlock (   //check equal block
+        .i_rs1(i_rs1Data),
+        .i_rs2(i_rs2Data),
+        .o_brEqual(o_brEqual)
+    );
+
+    branchLessControl branchLessBlock ( //check less than block
+        .i_rs1(rs1),
+        .i_rs2(rs2),
+        .i_brU(i_brUnsign),
+        .o_less(o_brLess)
+    );
 
 endmodule
 
-module xorLogic(
-    input logic [31:0] i_rs1;
-    input logic [31:0] i_rs2;
-    output logic o_brEqual
+module xorLogic (
+    input  [31:0] i_rs1,
+    input  [31:0] i_rs2,
+    output        o_brEqual
 );
-logic [31:0] xorR1R2;
-assign xorR1R2 = i_rs1 ^ i_rs2;
-assign o_brEqual = |xorR1R2;
+    wire [31:0] xorR1R2;
+
+    assign xorR1R2  = i_rs1 ^ i_rs2; 
+    assign o_brEqual = ~(|xorR1R2); 
+
 endmodule
 
-module branchLessControl(
-    input logic [32:0] i_rs1,
-    input logic [32:0] i_rs2,
-    input logic i_brU,
-    output logic o_less
+module branchLessControl (
+    input  [32:0] i_rs1,
+    input  [32:0] i_rs2,
+    input         i_brU,
+    output        o_less
 );
-logic [32:0] Prebu2Rs2;
-assign Prebu2Rs2 = ~i_rs2;
-logic [32:0] bu2Rs2;
-assign bu2Rs2 = Prebu2Rs2 + 33'd1;
-logic [32:0] subR1R2;
-assign subR1R2 = i_rs1 + bu2Rs2;
-assign o_less = subR1R2[32]&i_brU | subR1R2[31]&(~i_brU);
+    wire [32:0] bu2Rs2, subR1R2;
+
+    assign bu2Rs2  = ~i_rs2 + 1'b1; 
+    assign subR1R2 = i_rs1 + bu2Rs2;
+
+    assign o_less = (i_brU) ? subR1R2[32] : subR1R2[31];
+
 endmodule
